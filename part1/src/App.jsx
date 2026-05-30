8787319c54ff1e979b890fd6ccebd65714305cf2
Van Phan <khuevan2810@gmail.com>
2026-05-30 09:38:01 +0300

    1.1: Course Information, step 1

---
+++ part1/src/App.jsx
@@ -0,0 +1,31 @@
+import Header from './components/Header'
+import Content from './components/Content'
+import Total from './components/Total'
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const courseParts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={courseParts} />
+      <Total parts={courseParts} />
+    </div>
+  )
+}
+
+export default App
