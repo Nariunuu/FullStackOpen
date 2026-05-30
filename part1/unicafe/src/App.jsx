a5be70fdcb187a6d290b40943f5032807dda71bc
Van Phan <khuevan2810@gmail.com>
2026-05-30 10:02:42 +0300

    1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Button from './components/Button'
+import Statistics from './components/Statistics'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Unicafe</h1>
+      <Button text="good" onClick={() => setGood(good + 1)} />
+      <Button text="neutral" onClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" onClick={() => setBad(bad + 1)} />
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
---
+++ part1/unicafe/src/components/Statistics.jsx
@@ -0,0 +1,12 @@
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
+export default Statistics
